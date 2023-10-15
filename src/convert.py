50567d15ae851a797bf41118a423d961760aaf53
vorozhkog <vorozkhog@icloud.com>
2023-10-15 18:28:58 +0100

    conversion script done

--- src/convert.py
+++ src/convert.py
@@ -5,8 +5,11 @@ import src.settings as s
 from urllib.parse import unquote, urlparse
 from supervisely.io.fs import get_file_name, get_file_size
 import shutil
-
+from glob import glob
 from tqdm import tqdm
+import imagesize
+import xml.etree.ElementTree as ET
+
 
 def download_dataset(teamfiles_dir: str) -> str:
     """Use it for large datasets to convert them on the instance"""
@@ -29,7 +32,7 @@ def download_dataset(teamfiles_dir: str) -> str:
             total=fsize,
             unit="B",
             unit_scale=True,
-        ) as pbar:        
+        ) as pbar:
             api.file.download(team_id, teamfiles_path, local_path, progress_cb=pbar)
         dataset_path = unpack_if_archive(local_path)
 
@@ -57,7 +60,8 @@ def download_dataset(teamfiles_dir: str) -> str:
 
         dataset_path = storage_dir
     return dataset_path
-    
+
+
 def count_files(path, extension):
     count = 0
     for root, dirs, files in os.walk(path):
@@ -65,21 +69,166 @@ def count_files(path, extension):
             if file.endswith(extension):
                 count += 1
     return count
-    
+
+
+def xml_to_dict(xml_file_path):
+    try:
+        tree = ET.parse(xml_file_path)
+        root = tree.getroot()
+        return element_to_dict(root)
+    except Exception as e:
+        print(f"Error parsing XML: {e}")
+        return None
+
+
+def element_to_dict(element):
+    result = {}
+
+    if element.text:
+        result[element.tag] = element.text
+
+    for child in element:
+        child_data = element_to_dict(child)
+        if child.tag in result:
+            if type(result[child.tag]) is list:
+                result[child.tag].append(child_data)
+            else:
+                result[child.tag] = [result[child.tag], child_data]
+        else:
+            result[child.tag] = child_data
+
+    return result
+
+
+def create_ann(image_path):
+    labels = []
+    img_width, img_height = imagesize.get(image_path)
+    subdataset_path = os.path.dirname(os.path.dirname(os.path.dirname(image_path)))
+    image_path_splits = image_path.split("/")
+
+    ann_path = os.path.join(
+        subdataset_path,
+        image_path_splits[-3],
+        "Annotations",
+        (os.path.basename(image_path)[:-3] + "xml"),
+    )
+    # different folder structure, have to bidlocode
+
+    if image_path_splits[-4] == "ASAYAR_TXT":
+        ann_path = os.path.join(
+            subdataset_path,
+            "Annotations",
+            "Word-Level",
+            image_path_splits[-2],
+            (os.path.basename(image_path)[:-3] + "xml"),
+        )
+    ann_dict = xml_to_dict(ann_path)
+    if ann_dict is None:
+        print(f"Error:{image_path}")
+        return sly.Annotation(img_size=(img_height, img_width), labels=labels)
+    if type(ann_dict["object"]) == dict:
+        objct = []
+        objct.append(ann_dict["object"])
+    else:
+        objct = ann_dict["object"]
+    for obj in objct:
+        x_min = obj["bndbox"]["xmin"]["xmin"]
+        y_min = obj["bndbox"]["ymin"]["ymin"]
+        x_max = obj["bndbox"]["xmax"]["xmax"]
+        y_max = obj["bndbox"]["ymax"]["ymax"]
+
+        obj_class_name = obj["name"]["name"]
+        rectangle = sly.Rectangle(
+            top=int(y_min), left=int(x_min), bottom=int(y_max), right=int(x_max)
+        )
+        obj_class = obj_class_dict.get(obj_class_name)
+        if obj_class is None:
+            raise ValueError()
+
+        label = sly.Label(rectangle, obj_class)
+        labels.append(label)
+
+    tag_value = os.path.basename(subdataset_path).split("_")[1].lower()
+    subds_tag = sly.Tag(tm_subds, value=tag_value)
+
+    return sly.Annotation(img_size=(img_height, img_width), labels=labels, img_tags=[subds_tag])
+
+
+obj_class_dict = {
+    "Guide_Sign": sly.ObjClass("guide sign", geometry_type=sly.Rectangle, color=[0, 255, 0]),
+    "Regulatory_Sign": sly.ObjClass("regulatory sign", geometry_type=sly.Rectangle),
+    "Warning_Sign": sly.ObjClass("warning sign", geometry_type=sly.Rectangle),
+    "Down_Arrow": sly.ObjClass("down arrow", geometry_type=sly.Rectangle, color=[255, 255, 0]),
+    "Down_Left_Arrow": sly.ObjClass("down left arrow", geometry_type=sly.Rectangle),
+    "Down_Right_Arrow": sly.ObjClass(
+        "down right arrow", geometry_type=sly.Rectangle, color=[0, 255, 255]
+    ),
+    "Mixed_Arrow": sly.ObjClass("mixed arrow", geometry_type=sly.Rectangle, color=[255, 0, 0]),
+    "Right_Arrow": sly.ObjClass("right arrow", geometry_type=sly.Rectangle, color=[0, 0, 255]),
+    "Up_Arrow": sly.ObjClass("up arrow", geometry_type=sly.Rectangle, color=[255, 0, 255]),
+    "Arabic_Word": sly.ObjClass("arabic box", geometry_type=sly.Rectangle),
+    "Latin_Word": sly.ObjClass("latin box", geometry_type=sly.Rectangle),
+    "Number": sly.ObjClass("number", geometry_type=sly.Rectangle),
+    "Mixed_Word": sly.ObjClass("mixed box", geometry_type=sly.Rectangle),
+}
+tm_subds = sly.TagMeta("subdataset", sly.TagValueType.ANY_STRING)
+meta = sly.ProjectMeta(obj_classes=list(obj_class_dict.values()), tag_metas=[tm_subds])
+
+
 def convert_and_upload_supervisely_project(
     api: sly.Api, workspace_id: int, project_name: str
 ) -> sly.ProjectInfo:
-    ### Function should read local dataset and upload it to Supervisely project, then return project info.###
-    raise NotImplementedError("The converter should be implemented manually.")
+    project = api.project.create(workspace_id, project_name)
+    api.project.update_meta(project.id, meta.to_json())
 
-    # dataset_path = "/local/path/to/your/dataset" # general way
-    # dataset_path = download_dataset(teamfiles_dir) # for large datasets stored on instance
+    train_dirs = [
+        "/mnt/c/users/german/documents/ASAYAR_SIGN/Train/Images/*",
+        "/mnt/c/users/german/documents/ASAYAR_SYM/Train/Images/*",
+        "/mnt/c/users/german/documents/ASAYAR_TXT/Images/Train/*",
+    ]
+    test_dirs = [
+        "/mnt/c/users/german/documents/ASAYAR_SIGN/Test/Images/*",
+        "/mnt/c/users/german/documents/ASAYAR_SYM/Test/Images/*",
+        "/mnt/c/users/german/documents/ASAYAR_TXT/Images/Test/*",
+    ]
 
-    # ... some code here ...
+    test_images = []
+    for testdir in test_dirs:
+        test_img_pathes = glob(testdir)
+        for test_img_path in test_img_pathes:
+            test_images.append(test_img_path)
 
-    # sly.logger.info('Deleting temporary app storage files...')
-    # shutil.rmtree(storage_dir)
+    train_images = []
+    for dir in train_dirs:
+        train_img_pathes = glob(dir)
+        for train_img_path in train_img_pathes:
+            train_images.append(train_img_path)
 
-    # return project
+    ds_paths = {
+        "train": train_images,
+        "test": test_images,
+    }
 
+    batch_size = 50
+    for ds_name, images_pathes in ds_paths.items():
+        dataset = api.dataset.create(project.id, ds_name, change_name_if_conflict=True)
+        progress = sly.Progress("Create dataset {}".format(ds_name), len(images_pathes))
+        for img_pathes_batch in sly.batched(images_pathes, batch_size=batch_size):
+            # ds has the same filenames across every subdataset, so have to change each name
+            img_names_batch = [
+                (os.path.dirname(im_path)).split("/")[-3].split("_")[1].lower()
+                + "_"
+                + sly.fs.get_file_name_with_ext(im_path)
+                for im_path in img_pathes_batch
+            ]
 
+            img_infos = api.image.upload_paths(
+                dataset.id,
+                img_names_batch,
+                img_pathes_batch,
+            )
+            img_ids = [im_info.id for im_info in img_infos]
+
+            anns = [create_ann(image_path) for image_path in img_pathes_batch]
+            api.annotation.upload_anns(img_ids, anns)
+    return project
--- src/settings.py
+++ src/settings.py
@@ -13,31 +13,31 @@ from dataset_tools.templates import (
 ##################################
 # * Before uploading to instance #
 ##################################
-PROJECT_NAME: str = None
-PROJECT_NAME_FULL: str = None
+PROJECT_NAME: str = "ASAYAR"
+PROJECT_NAME_FULL: str = "ASAYAR: A Dataset for Arabic-Latin Text Detection"
 HIDE_DATASET = True  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
 ##################################
-LICENSE: License = None
-APPLICATIONS: List[Union[Industry, Domain, Research]] = None
-CATEGORY: Category = None
+LICENSE: License = License.Custom("https://vcar.github.io/ASAYAR/conditions/", redistributable=False)
+APPLICATIONS: List[Union[Industry, Domain, Research]] = [Domain.OCR(is_used=False)]
+CATEGORY: Category = Category.SelfDriving()
 
-CV_TASKS: List[CVTask] = None
-ANNOTATION_TYPES: List[AnnotationType] = None
+CV_TASKS: List[CVTask] = [CVTask.ObjectDetection()]
+ANNOTATION_TYPES: List[AnnotationType] = [AnnotationType.ObjectDetection()]
 
 RELEASE_DATE: Optional[str] = None  # e.g. "YYYY-MM-DD"
 if RELEASE_DATE is None:
-    RELEASE_YEAR: int = None
+    RELEASE_YEAR: int = 2021
 
-HOMEPAGE_URL: str = None
+HOMEPAGE_URL: str = "https://vcar.github.io/ASAYAR/"
 # e.g. "https://some.com/dataset/homepage"
 
 PREVIEW_IMAGE_ID: int = None
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
-GITHUB_URL: str = None
+GITHUB_URL: str = "https://github.com/dataset-ninja/asayar"
 # URL to GitHub repo on dataset ninja (e.g. "https://github.com/dataset-ninja/some-dataset")
 
 ##################################
@@ -51,12 +51,12 @@ CLASS2COLOR: Optional[Dict[str, List[str]]] = None
 
 # If you have more than the one paper, put the most relatable link as the first element of the list
 # Use dict key to specify name for a button
-PAPER: Optional[Union[str, List[str], Dict[str, str]]] = None
+PAPER: Optional[Union[str, List[str], Dict[str, str]]] = "https://ieeexplore.ieee.org/document/9233923"
 BLOGPOST: Optional[Union[str, List[str], Dict[str, str]]] = None
 REPOSITORY: Optional[Union[str, List[str], Dict[str, str]]] = None
 
-CITATION_URL: Optional[str] = None
-AUTHORS: Optional[List[str]] = None
+CITATION_URL: Optional[str] = "https://vcar.github.io/ASAYAR/"
+AUTHORS: Optional[List[str]] = ["M. Akallouch", "K. S. Boujemaa", "A. Bouhoute", "K. Fardousse", "I. Berrada"]
 
 ORGANIZATION_NAME: Optional[Union[str, List[str]]] = None
 ORGANIZATION_URL: Optional[Union[str, List[str]]] = None
